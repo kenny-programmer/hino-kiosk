2dcc2d13d692e0689dce3a606bf3cc31c61a738c
kenny-programmer <roxas.vctr@gmail.com>
2025-06-08 16:15:16 +0800

    fix css

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        border: 'hsl(var(--border))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+      },
+      keyframes: {
+        banner: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+      },
+      animation: {
+        banner: 'banner 60s linear infinite',
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
